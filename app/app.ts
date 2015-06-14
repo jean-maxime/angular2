75f443540dc7ac0a72d7a94d6f02cb8953e8616c
ksomville <kirgan.somville@hetic.net>
2015-06-15 00:11:10 +0200

    router

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,23 @@
 /// <reference path="../typings/tsd.d.ts" />
 import {Component, View, bootstrap} from 'angular2/angular2';
+import {RouteConfig, RouterOutlet, RouterLink, routerInjectables} from 'angular2/router';
+
+import {Search} from './components/search/search';
+import {Random} from './components/random/random';
 
 @Component({
   selector: 'app'
 })
+@RouteConfig([
+  { path: '/search', component: Search, as: 'search' },
+  { path: '/random', component: Random, as: 'random' }
+])
 @View({
-  template: '<h1>Welcome !</h1>'
+  template: '<h1>Welcome !</h1>',
+  directives: [RouterOutlet, RouterLink]
 })
 class App {
 
 }
 
-bootstrap(App);
+bootstrap(App, [routerInjectables]);
---
+++ dist/dev/components/search/search.js
@@ -0,0 +1,27 @@
+if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+if (typeof __metadata !== "function") __metadata = function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var angular2_1 = require('angular2/angular2');
+var Search = (function () {
+    function Search() {
+    }
+    Search = __decorate([
+        angular2_1.Component({}),
+        angular2_1.View({
+            template: '<h1>Welcome page search !</h1>'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], Search);
+    return Search;
+})();
+exports.Search = Search;
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvc2VhcmNoL3NlYXJjaC50cyJdLCJuYW1lcyI6WyJTZWFyY2giLCJTZWFyY2guY29uc3RydWN0b3IiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBQUEseUJBQThCLG1CQUFtQixDQUFDLENBQUE7QUFFbEQ7SUFBQUE7SUFRb0JDLENBQUNBO0lBUnJCRDtRQUFDQSxvQkFBU0EsQ0FBQ0EsRUFFVkEsQ0FBQ0E7UUFDREEsZUFBSUEsQ0FBQ0E7WUFDSkEsUUFBUUEsRUFBRUEsZ0NBQWdDQTtTQUMzQ0EsQ0FBQ0E7O2VBR21CQTtJQUFEQSxhQUFDQTtBQUFEQSxDQVJwQixBQVFxQkEsSUFBQTtBQUFSLGNBQU0sU0FBRSxDQUFBIiwiZmlsZSI6ImNvbXBvbmVudHMvc2VhcmNoL3NlYXJjaC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7Q29tcG9uZW50LCBWaWV3fSBmcm9tICdhbmd1bGFyMi9hbmd1bGFyMic7XG5cbkBDb21wb25lbnQoe1xuXG59KVxuQFZpZXcoe1xuICB0ZW1wbGF0ZTogJzxoMT5XZWxjb21lIHBhZ2Ugc2VhcmNoICE8L2gxPidcbn0pXG5cblxuZXhwb3J0IGNsYXNzIFNlYXJjaHt9XG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
