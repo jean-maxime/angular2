83cb399dc3d8c732a7b09a8de0b42bb0a786d660
Jean-Maxime <jean-max@wanadoo.fr>
2015-06-18 21:35:23 +0200

    mise en place du formulaire

--- app/components/search/search.ts
+++ app/components/search/search.ts
@@ -1,11 +1,29 @@
-import {Component, View} from 'angular2/angular2';
+import {Component, View, bootstrap, formDirectives, ControlGroup, Control, Validators} from 'angular2/angular2';
+import {XHR} from 'angular2/src/core/compiler/xhr/xhr';
 
 @Component({
-
+	selector: 'search'
 })
 @View({
-  template: '<h1>Welcome page search !</h1>'
+	directives: [formDirectives],
+  	templateUrl: './components/search/search.html'
 })
 
+export class Search{
+	searchForm:ControlGroup;
 
-export class Search{}
+	constructor() {
+		this.searchForm = new ControlGroup({
+			card: new Control("", Validators.required), // pre-existing validator
+		});
+	}
+
+	onSearch(e) {
+		e.preventDefault();
+		if(this.searchForm.valid) { // return true or false, depending on the form state
+			console.log("valid data, do something with it ->", this.searchForm.value.card);
+		} else {
+			console.error("invalid form", this.searchForm);
+		}
+	}
+}
