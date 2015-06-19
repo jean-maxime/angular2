1a349f3b5bcae21aeeb7ebd32809b893f71075fa
Jean-Maxime <jean-max@wanadoo.fr>
2015-06-19 23:32:51 +0200

    correction du service

--- app/services/hearthstoneApi.ts
+++ app/services/hearthstoneApi.ts
@@ -1,23 +1,24 @@
 export class HearthstoneApi {
 
-
+	findCard: String;
 
 	getDatabase(card){
 
-		var findCard = "https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/"+card;
-		  return new Promise((resolve, reject) => {
-	      fetch(finCard, {
-	      	headers:{
-	      		"X-Mashape-Key": "ojOTIgJwAGmshRKAl9MT4bt94nGrp1BlUj5jsnlYR5NDmN2G6q"
-	      	}
-	 	 })
-	      .then(response => response.json())
-	      .then(response => {
-	        resolve(response); // resolve promise with response if it fetch succeded
-	        console.log('from back'+response)
-	      }).catch(() => {
-	        reject(); // reject promise if we catch a fetch error
-	      });
-	    });
+		findCard = "https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/"+card;
+	 	return new Promise((resolve, reject) => {
+     		fetch(findCard, {
+	      		headers:{
+	      			"X-Mashape-Key": "ojOTIgJwAGmshRKAl9MT4bt94nGrp1BlUj5jsnlYR5NDmN2G6q"
+	      		}
+ 	 		})
+      		.then(response => response.json())
+      		.then(response => {
+        		resolve(response); // resolve promise with response if it fetch succeded
+        		console.log('from back'+response)
+      		}).catch(() => {
+        		reject(); // reject promise if we catch a fetch error
+     		});
+    	});
+	}
 
 }
